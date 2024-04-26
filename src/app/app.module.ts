69171614b9b77a10e72dff3a87133052c7dcfe4e
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-04-25 23:18:24 -0400

    23:56 on tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import { DialogComponent } from './dialog/dialog.component';
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import { DialogComponent } from './dialog/dialog.component';
     MatIcon,
     MatIconButton,
     MatButton,
-    MatDialogModule
+    MatDialogModule,
+    MatFormField,
+    MatInput,
+    MatFormFieldModule
   ],
   providers: [
     provideClientHydration(),
